d549d6ea10b3d674b2fb9c4175413527914f1ad4
Kyle Greenidge <kylegreen149@gmail.com>
2025-07-14 16:59:28 -0400

    Fix other light/dark mode features with navbar, buttons, and logo

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ const themeToggle = document.querySelector(".theme-toggle");
 
 const body = document.querySelector("body")
 
+const logoImg = document.querySelector(".page-info-header img");
+
+const updateImage = () => {
+    if (body.classList.contains("dark-theme")) {
+        logoImg.src = logoImg.getAttribute("data-dark");
+        
+    } else {
+        logoImg.src = logoImg.getAttribute("data-light");
+    }
+};
+
 const changeTheme = () => {
     body.classList.toggle("dark-theme");
     body.classList.toggle("light-theme");
@@ -14,6 +25,7 @@ const changeTheme = () => {
         themeToggle.textContent = "Toggle Dark Mode";
         console.log("LIGHT MODE!!");
     }
+    updateImage()
 };
 
 themeToggle.addEventListener("click", changeTheme)
