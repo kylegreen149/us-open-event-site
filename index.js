e4b5240af74f6acdf9639c84d5df369fffc9bbc6
Kyle Greenidge <kylegreen149@gmail.com>
2025-07-15 01:52:00 -0400

    Add top 15 players on mens and womens side

--- index.js
+++ index.js
@@ -29,3 +29,259 @@ const changeTheme = () => {
 };
 
 themeToggle.addEventListener("click", changeTheme)
+
+// Toggle between Top 15 Mens and Top 15 Womens Players
+
+ const mensTop15 = [
+    {
+        "first-name": "Jannik",
+        "last-name": "Sinner",
+        "country": "ITA",
+        "flag": "🇮🇹",
+        "age": 23,
+        "rank": 1
+    },
+    {
+        "first-name": "Carlos",
+        "last-name": "Alcaraz",
+        "country": "ESP",
+        "flag": "🇪🇸",
+        "age": 22,
+        "rank": 2
+    },
+    {
+        "first-name": "Alexander",
+        "last-name": "Zverev",
+        "country": "GER",
+        "flag": "🇩🇪",
+        "age": 28,
+        "rank": 3
+    },
+    {
+        "first-name": "Taylor",
+        "last-name": "Fritz",
+        "country": "USA",
+        "flag": "🇺🇸",
+        "age": 28,
+        "rank": 4
+    },
+    {
+        "first-name": "Jack",
+        "last-name": "Draper",
+        "country": "GBR",
+        "flag": "🇬🇧",
+        "age": 23,
+        "rank": 5
+    },
+    {
+        "first-name": "Novak",
+        "last-name": "Djokovic",
+        "country": "SER",
+        "flag": "🇷🇸",
+        "age": 38,
+        "rank": 6
+    },
+    {
+        "first-name": "Lorenzo",
+        "last-name": "Musetti",
+        "country": "ITA",
+        "flag": "🇮🇹",
+        "age": 23,
+        "rank": 7
+    },
+    {
+        "first-name": "Holger",
+        "last-name": "Rune",
+        "country": "DEN",
+        "flag": "🇩🇰",
+        "age": 22,
+        "rank": 8
+    },
+    {
+        "first-name": "Ben",
+        "last-name": "Shelton",
+        "country": "USA",
+        "flag": "🇺🇸",
+        "age": 22,
+        "rank": 9
+    },
+    {
+        "first-name": "Andrey",
+        "last-name": "Rublev",
+        "country": "RUS",
+        "flag": "🇷🇺",
+        "age": 27,
+        "rank": 10
+    },
+    {
+        "first-name": "Frances",
+        "last-name": "Tiafoe",
+        "country": "USA",
+        "flag": "🇺🇸",
+        "age": 27,
+        "rank": 11
+    },
+    {
+        "first-name": "Alex",
+        "last-name": "de Minaur",
+        "country": "AUS",
+        "flag": "🇦🇺",
+        "age": 26,
+        "rank": 12
+    },
+    {
+        "first-name": "Casper",
+        "last-name": "Rudd",
+        "country": "NOR",
+        "flag": "🇳🇴",
+        "age": 26,
+        "rank": 13
+    },
+    {
+        "first-name": "Daniil",
+        "last-name": "Medvedev",
+        "country": "RUS",
+        "flag": "🇷🇺",
+        "age": 29,
+        "rank": 14
+    },
+    {
+        "first-name": "Arthur",
+        "last-name": "Fils",
+        "country": "FRA",
+        "flag": "🇫🇷",
+        "age": 21,
+        "rank": 15
+    }
+]
+
+const womensTop15 = [
+    {
+        "first-name": "Aryna",
+        "last-name": "Sabalenka",
+        "country": "BLR",
+        "flag": "🇧🇾",
+        "age": 27,
+        "rank": 1
+    },
+    {
+        "first-name": "Coco",
+        "last-name": "Gauff",
+        "country": "USA",
+        "flag": "🇺🇸",
+        "age": 21,
+        "rank": 2
+    },
+    {
+        "first-name": "Iga",
+        "last-name": "Swiatek",
+        "country": "POL",
+        "flag": "🇵🇱",
+        "age": 28,
+        "rank": 3
+    },
+    {
+        "first-name": "Jessica",
+        "last-name": "Pegula",
+        "country": "USA",
+        "flag": "🇺🇸",
+        "age": 31,
+        "rank": 4
+    },
+    {
+        "first-name": "Mirra",
+        "last-name": "Andreeva",
+        "country": "RUS",
+        "flag": "🇷🇺",
+        "age": 18,
+        "rank": 5
+    },
+    {
+        "first-name": "Qinweng",
+        "last-name": "Zheng",
+        "country": "CHN",
+        "flag": "🇨🇳",
+        "age": 22,
+        "rank": 6
+    },
+    {
+        "first-name": "Amanda",
+        "last-name": "Anisimova",
+        "country": "USA",
+        "flag": "🇺🇸",
+        "age": 23,
+        "rank": 7
+    },
+    {
+        "first-name": "Madison",
+        "last-name": "Keys",
+        "country": "USA",
+        "flag": "🇺🇸",
+        "age": 30,
+        "rank": 8
+    },
+    {
+        "first-name": "Jasmine",
+        "last-name": "Paolini",
+        "country": "ITA",
+        "flag": "🇮🇹",
+        "age": 29,
+        "rank": 9
+    },
+    {
+        "first-name": "Paula",
+        "last-name": "Badosa",
+        "country": "ESP",
+        "flag": "🇪🇸",
+        "age": 27,
+        "rank": 10
+    },
+    {
+        "first-name": "Emma",
+        "last-name": "Navaro",
+        "country": "USA",
+        "flag": "🇺🇸",
+        "age": 24,
+        "rank": 11
+    },
+    {
+        "first-name": "Karolina",
+        "last-name": "Muchova",
+        "country": "CZE",
+        "flag": "🇨🇿",
+        "age": 28,
+        "rank": 12
+    },
+    {
+        "first-name": "Elena",
+        "last-name": "Rybakina",
+        "country": "KAZ",
+        "flag": "🇰🇿",
+        "age": 26,
+        "rank": 13
+    },
+    {
+        "first-name": "Elina",
+        "last-name": "Svitolina",
+        "country": "UKR",
+        "flag": "🇺🇦",
+        "age": 30,
+        "rank": 14
+    },
+    {
+        "first-name": "Diana",
+        "last-name": "Shnaider",
+        "country": "RUS",
+        "flag": "🇷🇺",
+        "age": 21,
+        "rank": 15
+    }
+]
+
+for (player of mensTop15) {
+    console.log(player["first-name"] + " " + player["last-name"] + " from " + player.country + " is ranked #" + player.rank + " and is " + player.age + " years old.");
+}
+console.log("")
+for (player of womensTop15) {
+    console.log(player["first-name"] + " " + player["last-name"] + " from " + player.country + " is ranked #" + player.rank + " and is " + player.age + " years old.");
+}
